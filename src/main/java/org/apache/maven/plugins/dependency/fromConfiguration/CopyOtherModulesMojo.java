7259795f1e58290117874da66d57b4a237e0942a
Gregory Anne <englishblazere@free.fr>
2018-12-09 19:18:16 +0100

    enriching the pom.xml

--- src/main/java/org/apache/maven/plugins/dependency/fromConfiguration/CopyOtherModulesMojo.java
+++ src/main/java/org/apache/maven/plugins/dependency/fromConfiguration/CopyOtherModulesMojo.java
@@ -10,11 +10,13 @@ import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.StringUtils;
 
-//CHECKSTYLE_OFF: LineLength
-@Mojo(name = "copy-modules", requiresDependencyResolution = ResolutionScope.TEST, defaultPhase = LifecyclePhase.PROCESS_SOURCES, threadSafe = true)
-//CHECKSTYLE_ON: LineLength
-public class CopyOtherModulesMojo
-        extends CopyMojo {
+/**
+ * This mojo helps copying the group dependencies to the current target
+ * @author zg2pro
+ */
+@Mojo(name = "copy-modules", requiresDependencyResolution = ResolutionScope.TEST, 
+        defaultPhase = LifecyclePhase.PROCESS_SOURCES, threadSafe = true)
+public class CopyOtherModulesMojo extends CopyMojo {
 
     @Override
     protected void doExecute()
